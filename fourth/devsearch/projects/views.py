4461c29a5baa5392cf5f7efa4f02c66aa107b5d2
MashaAvdulova <avdulova@gmail.com>
2025-08-18 22:03:41 +0300

    250818-2

--- fourth/devsearch/projects/views.py
+++ fourth/devsearch/projects/views.py
@@ -23,12 +23,14 @@ def project(request, pk):
 
 @login_required(login_url='login')
 def create_project(request):
+    profile = request.user.profile
     form = ProjectForm()
 
     if request.method == "POST":
         form = ProjectForm(request.POST, request.FILES)
         if form.is_valid():
             project = form.save(commit=False)
+            project.owner = profile
             form.save()
             return redirect('projects')
 
